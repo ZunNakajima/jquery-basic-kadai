a8b90bbdd9ae3017e76f6d9719857f6903152594
ZunNakajima <zunnakajima.it@gmail.com>
2025-06-30 08:35:37 +0900

    Delete event.js

--- event.js
+++
@@ -1,9 +0,0 @@
-//HTMLの読み込みが完了したら
-$(window).on('load', function(){
-  console.log('loadイベントが発生しました');
-});
-
-//画面をスクロールしたら
-$(window).on('scroll', function(){
-  console.log('scrollイベントが発生しました')
-});
